97ab786030d1796d85aea05113df4ecb76bb5069
shawn3298317 <shawn3298317lin@gmail.com>
2019-10-08 22:26:31 -0400

    ROC curve looks like shit

--- pa1/main.py
+++ pa1/main.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
 
     # Hyper param settings
     args = {
-        "NNodes": 3,
+        "NNodes": 10,
         "activate": relu,
         "deltaActivate": d_relu,
-        "learningRate": 0.015,
-        "epochs": 5,
-        "regLambda": 0.01,
+        "learningRate": 0.0015,
+        "epochs": 200,
+        "regLambda": 0.00,
         "batchSize": 20,
         "task": TASK_MULTI_CLASS
         # "task": "regression"
@@ -24,10 +24,10 @@ if __name__ == "__main__":
         Logging.info("\t%s: %s" % (k, v))
 
     # Data prep.
-    # X, Y = getData("data/DataFor640/dataset1/", "LinearX.csv", "LinearY.csv")
+    X, Y = getData("data/DataFor640/dataset1/", "LinearX.csv", "LinearY.csv")
     # X, Y = getData("data/DataFor640/dataset1/", "NonLinearX.csv", "NonLinearY.csv")
-    X, Y = getData("data/DataFor640/dataset2/", "Digit_x.csv", "Digit_y.csv")
-    train_ind, test_ind = splitData(X, Y, 2)
+    # X, Y = getData("data/DataFor640/dataset2/", "Digit_x.csv", "Digit_y.csv")
+    train_ind, test_ind = splitData(X, Y, 5)
     X_train, Y_train = X[train_ind], Y[train_ind]
     X_test, Y_test = X[test_ind], Y[test_ind]
     Logging.info("X train shape: {}".format(X_train.shape))
@@ -35,19 +35,17 @@ if __name__ == "__main__":
     Logging.info("X test shape: {}".format(X_test.shape))
     Logging.info("Y test shape: {}".format(Y_test.shape))
     model = train(X_train, Y_train, args)
-    test_labels = test(X_test, model)
+    # test_labels = test(X_test, model)
+    y_predict = model.predict(X_test)
 
     # Logging.info("test_labels={}".format(test_labels))
     # metrics = getPerformanceScores(Y_test, test_labels)
     # Logging.info("Performance metrics: {}".format(metrics))
 
-    if args["task"] != "regression":
-        metrics = getPerformanceScores(Y_test, test_labels)
-        Logging.info("Performance metrics: {}".format(metrics))
-        plts = get_plot_ROC(model,X_test,Y_test)
-        for plt in plts:
-            plt.show()
-    else:
-        y_predict = model.predict(X_test)
-        test_cost = model.getCost(Y_test, y_predict)
-        Logging.info("Test Loss: %.3f" % test_cost)
+    metrics = getPerformanceScores(Y_test, y_predict)
+    Logging.info("Performance metrics: {}".format(metrics))
+    plts = get_plot_ROC(model,X_test,Y_test)
+    for plt in plts:
+        plt.show()
+    test_cost = model.getCost(Y_test, y_predict)
+    Logging.info("Test Loss: %.3f" % test_cost)
